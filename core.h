3207f2ad961dc48c357f32fa280735e8d0c8e56e
Bo Rydberg <bolry@hotmail.com>
2013-05-05 23:57:51 +0200

    added some basic testing with cmake

--- core.h
+++ core.h
@@ -4,4 +4,12 @@
 #error "CORE_H"
 #endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int parse_bool_flag(char const unknown_arg[], char const option[], int unique_from);
+
+#ifdef __cplusplus
+}
+#endif
