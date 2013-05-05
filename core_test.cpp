79880f7068d04c7becf41a27fd2eef69b94647f5
Bo Rydberg <bolry@hotmail.com>
2013-05-05 23:56:33 +0200

    added som basic testing with cmake

---
+++ core_test.cpp
@@ -0,0 +1,105 @@
+#include "core.h"
+
+#include <gmock/gmock.h>
+
+static char const empty[0+1] = "";
+static char const another_empty[0+1] = "";
+static char const dash[] = "-";
+static char const another_dash[] = "-";
+static char const str_one[1+1] = "a";
+static char const str_two[2+1] = "bc";
+
+TEST(ParseBoolFlagWithEmptyTest, EmptyExact)
+{
+	EXPECT_NE(empty, another_empty);
+	EXPECT_TRUE(parse_bool_flag(empty, another_empty, -1));
+	EXPECT_TRUE(parse_bool_flag(empty, empty, -1));
+}
+TEST(ParseBoolFlagWithEmptyTest, EmptyToSomethingExact)
+{
+	EXPECT_FALSE(parse_bool_flag(empty, str_one, -1));
+	EXPECT_FALSE(parse_bool_flag(str_one, empty, -1));
+	EXPECT_FALSE(parse_bool_flag(empty, str_two, -1));
+	EXPECT_FALSE(parse_bool_flag(str_two, empty, -1));
+}
+TEST(ParseBoolFlagWithEmptyTest, EmptyZero)
+{
+	EXPECT_NE(empty, another_empty);
+	EXPECT_TRUE(parse_bool_flag(empty, another_empty, 0));
+	EXPECT_TRUE(parse_bool_flag(empty, empty, 0));
+}
+TEST(ParseBoolFlagWithEmptyTest, EmptyToSomethingZero)
+{
+	EXPECT_TRUE(parse_bool_flag(empty, str_one, 0));
+	EXPECT_FALSE(parse_bool_flag(str_one, empty, 0));
+	EXPECT_TRUE(parse_bool_flag(empty, str_two, 0));
+	EXPECT_FALSE(parse_bool_flag(str_two, empty, 0));
+}
+TEST(ParseBoolFlagWithEmptyTest, EmptyToFirstChar)
+{
+	EXPECT_NE(empty, another_empty);
+	EXPECT_FALSE(parse_bool_flag(empty, another_empty, 1));
+	EXPECT_FALSE(parse_bool_flag(empty, empty, 1));
+}
+TEST(ParseBoolFlagWithEmptyTest, EmptyToSomethingToFirstChar)
+{
+	EXPECT_FALSE(parse_bool_flag(empty, str_one, 1));
+	EXPECT_FALSE(parse_bool_flag(str_one, empty, 1));
+	EXPECT_FALSE(parse_bool_flag(empty, str_two, 1));
+	EXPECT_FALSE(parse_bool_flag(str_two, empty, 1));
+}
+/*
+ *
+ */
+TEST(ParseBoolFlagWithDashTest, Exact)
+{
+	EXPECT_NE(dash, another_dash);
+	EXPECT_TRUE(parse_bool_flag(dash, another_dash, -1));
+	EXPECT_TRUE(parse_bool_flag(dash, dash, -1));
+}
+TEST(ParseBoolFlagWithDashTest, DashoSomethingExact)
+{
+	EXPECT_FALSE(parse_bool_flag(dash, str_one, -1));
+	EXPECT_FALSE(parse_bool_flag(str_one, dash, -1));
+	EXPECT_FALSE(parse_bool_flag(dash, str_two, -1));
+	EXPECT_FALSE(parse_bool_flag(str_two, dash, -1));
+}
+TEST(ParseBoolFlagWithDashTest, DashZero)
+{
+	EXPECT_NE(dash, another_dash);
+	EXPECT_TRUE(parse_bool_flag(dash, another_dash, 0));
+	EXPECT_TRUE(parse_bool_flag(dash, dash, 0));
+}
+TEST(ParseBoolFlagWithDashTest, DashToSomethingZero)
+{
+	EXPECT_FALSE(parse_bool_flag(dash, str_one, 0));
+	EXPECT_FALSE(parse_bool_flag(str_one, dash, 0));
+	EXPECT_FALSE(parse_bool_flag(dash, str_two, 0));
+	EXPECT_FALSE(parse_bool_flag(str_two, dash, 0));
+}
+TEST(ParseBoolFlagWithDashTest, DashToFirstChar)
+{
+	EXPECT_NE(dash, another_dash);
+	EXPECT_TRUE(parse_bool_flag(dash, another_dash, 1));
+	EXPECT_TRUE(parse_bool_flag(dash, dash, 1));
+}
+TEST(ParseBoolFlagWithDashTest, DashToSomethingToFirstChar)
+{
+	EXPECT_FALSE(parse_bool_flag(dash, str_one, 1));
+	EXPECT_FALSE(parse_bool_flag(str_one, dash, 1));
+	EXPECT_FALSE(parse_bool_flag(dash, str_two, 1));
+	EXPECT_FALSE(parse_bool_flag(str_two, dash, 1));
+}
+/*
+ *
+ */
+TEST(ParseBoolFlagWithDashTest, DashToDashieExact)
+{
+	EXPECT_FALSE(parse_bool_flag(dash, "-a", -1));
+	EXPECT_FALSE(parse_bool_flag("-a", dash, -1));
+}
+TEST(ParseBoolFlagWithDashTest, DashToDashieFirstChar)
+{
+	EXPECT_TRUE(parse_bool_flag(dash, "-a", 1));
+	EXPECT_FALSE(parse_bool_flag("-a", dash, 1));
+}
